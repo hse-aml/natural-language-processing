a299dabc39bada4013d25be23a7fcc388b28294f
Anna Potapenko <lilyevans60@gmail.com>
2018-01-18 21:28:45 +0100

    Project assignments added.

---
+++ project/dialogue_manager.py
@@ -0,0 +1,80 @@
+import os
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+
+from chatterbot import ChatBot
+from utils import *
+
+
+class ThreadRanker(object):
+    def __init__(self, paths):
+        self.word_embeddings, self.embeddings_dim = load_embeddings(paths['WORD_EMBEDDINGS'])
+        self.thread_embeddings_folder = paths['THREAD_EMBEDDINGS_FOLDER']
+
+    def __load_embeddings_by_tag(self, tag_name):
+        embeddings_path = os.path.join(self.thread_embeddings_folder, tag_name + ".pkl")
+        thread_ids, thread_embeddings = unpickle_file(embeddings_path)
+        return thread_ids, thread_embeddings
+
+    def get_best_thread(self, question, tag_name):
+        """ Returns id of the most similar thread for the question.
+            The search is performed across the threads with a given tag.
+        """
+        thread_ids, thread_embeddings = self.__load_embeddings_by_tag(tag_name)
+
+        # HINT: you have already implemented a similar routine in the 3rd assignment.
+        
+        question_vec = #### YOUR CODE HERE ####
+        best_thread = #### YOUR CODE HERE ####
+        
+        return thread_ids[best_thread]
+
+
+class DialogueManager(object):
+    def __init__(self, paths):
+        print("Loading resources...")
+
+        # Intent recognition:
+        self.intent_recognizer = unpickle_file(paths['INTENT_RECOGNIZER'])
+        self.tfidf_vectorizer = unpickle_file(paths['TFIDF_VECTORIZER'])
+
+        self.ANSWER_TEMPLATE = 'I think its about %s\n This thread might help you: https://stackoverflow.com/questions/%s'
+
+        # Goal-oriented part:
+        self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
+        self.thread_ranker = ThreadRanker(paths)
+
+    def create_chitchat_bot(self):
+        """Initializes self.chitchat_bot with some conversational model."""
+
+        # Hint: you might want to create and train chatterbot.ChatBot here.
+        
+        ########################
+        #### YOUR CODE HERE ####
+        ########################
+       
+    def generate_answer(self, question):
+        """Combines stackoverflow and chitchat parts using intent recognition."""
+
+        # Recognize intent of the question using `intent_recognizer`.
+        # Don't forget to prepare question and calculate features for the question.
+        
+        prepared_question = #### YOUR CODE HERE ####
+        features = #### YOUR CODE HERE ####
+        intent = #### YOUR CODE HERE ####
+
+        # Chit-chat part:   
+        if intent == 'dialogue':
+            # Pass question to chitchat_bot to generate a response.       
+            response = #### YOUR CODE HERE ####
+            return response
+        
+        # Goal-oriented part:
+        else:        
+            # Pass features to tag_clasifier to get predictions.
+            tag = #### YOUR CODE HERE ####
+            
+            # Pass prepared_question to thread_ranker to get predictions.
+            thread_id = #### YOUR CODE HERE ####
+           
+            return self.ANSWER_TEMPLATE % (tag, thread_id)
+
---
+++ project/main_bot.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import requests
+import time
+import argparse
+import os
+
+from requests.compat import urljoin
+
+
+class BotHandler(object):
+    """
+        BotHandler is a class which implements all back-end of the bot.
+        It has tree main functions:
+            'get_updates' — checks for new messages
+            'send_message' – posts new message to user
+            'get_answer' — computes the most relevant on a user's question
+    """
+
+    def __init__(self, token, dialogue_manager):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.dialogue_manager = dialogue_manager
+
+    def get_updates(self, offset=None, timeout=30):
+        params = {"timeout": timeout, "offset": offset}
+        resp = requests.get(urljoin(self.api_url, "getUpdates"), params).json()
+        if "result" not in resp:
+            return []
+        return resp["result"]
+
+    def send_message(self, chat_id, text):
+        params = {"chat_id": chat_id, "text": text}
+        return requests.post(urljoin(self.api_url, "sendMessage"), params)
+
+    def get_answer(self, question):
+        if question == '/start':
+            return "Hi, I am your project bot. How can I help you today?"
+        return self.dialogue_manager.generate_answer(question)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, default='')
+    return parser.parse_args()
+
+
+def is_unicode(text):
+    return len(text) == len(text.encode())
+
+
+class SimpleDialogueManager(object):
+    """
+    This is the simplest dialogue manager to test the telegram bot.
+    Your task is to create a more advanced one in dialogue_manager.py."
+    """
+    
+    def generate_answer(self, question): 
+        return "Hello, world!" 
+        
+
+def main():
+    args = parse_args()
+    token = args.token
+
+    if not token:
+        if not "TELEGRAM_TOKEN" in os.environ:
+            print("Please, set bot token through --token or TELEGRAM_TOKEN env variable")
+            return
+        token = os.environ["TELEGRAM_TOKEN"]
+
+    #################################################################
+    
+    # Your task is to complete dialogue_manager.py and use your 
+    # advanced DialogueManager instead of SimpleDialogueManager. 
+    
+    # This is the point where you plug it into the Telegram bot. 
+    # Do not forget to import all needed dependendies when you do so.
+    
+    simple_manager = SimpleDialogueManager()
+    bot = BotHandler(token, simple_manager)
+    
+    ###############################################################
+
+    print("Ready to talk!")
+    offset = 0
+    while True:
+        updates = bot.get_updates(offset=offset)
+        for update in updates:
+            print("An update received.")
+            if "message" in update:
+                chat_id = update["message"]["chat"]["id"]
+                if "text" in update["message"]:
+                    text = update["message"]["text"]
+                    if is_unicode(text):
+                        print("Update content: {}".format(update))
+                        bot.send_message(chat_id, bot.get_answer(update["message"]["text"]))
+                    else:
+                        bot.send_message(chat_id, "Hmm, you are sending some weird characters to me...")
+            offset = max(offset, update['update_id'] + 1)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
