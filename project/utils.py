abb30b75eb219e63914022eee7cded47329f395a
Anton Belyy <anton.belyy@gmail.com>
2019-06-16 23:03:54 +0300

    Add separate requirements.txt for Colab

--- project/utils.py
+++ project/utils.py
@@ -18,7 +18,7 @@ RESOURCE_PATH = {
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-    
+
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
@@ -41,7 +41,7 @@ def load_embeddings(embeddings_path):
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-    
+
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
@@ -59,7 +59,7 @@ def load_embeddings(embeddings_path):
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
-    
+
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
     ########################
--- setup_google_colab.py
+++ setup_google_colab.py
@@ -9,24 +9,15 @@ def download_github_code(path):
 
 
 def setup_common():
-    os.system("pip install tqdm")
-    os.system("pip install backports.weakref==1.0.post1")
-    os.system("pip install ChatterBot==0.7.6")
-    os.system("pip install enum34==1.1.6")
-    os.system("pip install funcsigs==1.0.2")
-    os.system("pip install gensim==3.1.0")
-    os.system("pip install jedi==0.11.0")
-    os.system("pip install libarchive==0.4.4")
-    os.system("pip install mock==2.0.0")
-    os.system("pip install parso==0.1.0")
-    os.system("pip install pbr==3.1.1")
-    os.system("pip install regex==2017.11.9")
-
+    download_github_code("common/requirements_colab.txt")
     download_github_code("common/download_utils.py")
     download_github_code("common/tqdm_utils.py")
     download_github_code("common/__init__.py")
     os.system("mkdir common")
     os.system("mv download_utils.py tqdm_utils.py __init__.py common/")
+    os.system("mv requirements_colab.txt common/")
+
+    os.system("pip install -r common/requirements_colab.txt --force-reinstall")
 
 
 def setup_starspace():
